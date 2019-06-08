12b0a4844a366319f90cae44b58ae9bbb188bae8
diehlms <diehlstx@gmail.com>
2019-06-08 13:15:27 -0500

    updating stylings and javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,17 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+    $('.navbar .dropdown').hover(function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+    }, function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+    });
+})
